c343b0e758ebd253a30af121311b8de4a682a462
Sunil <sunisimar2002d@gmail.com>
2024-01-30 16:11:28 +0530

    added cluster for mutli-core

---
+++ src/index.ts
@@ -0,0 +1,18 @@
+import cluster from "cluster";
+import os from "os";
+import { app } from "./express-app";
+
+const port = 3000;
+
+const cpuCount = os.cpus().length;
+
+if (cluster.isMaster) {
+  //Worker for each CPU
+  for (let i = 0; i < cpuCount; i++) {
+    cluster.fork();
+  }
+} else {
+  app.listen(port, () => {
+    console.log("listening on port 3000");
+  });
+}
